315a4e1b4449b95fb9fbaab8e635d93ae19b878c
Kasper Welbers <kasperwelbers@gmail.com>
2022-09-30 09:23:54 +0200

    resolved key error

--- components/PeopleList.tsx
+++ components/PeopleList.tsx
@@ -1,33 +1,10 @@
 import { NextPage } from "next";
-import { FaRegFolder } from "react-icons/fa";
 import { ReactElement } from "react-markdown/lib/react-markdown";
 import { Person } from "../types";
 
 interface Props {
   roles: { [role: string]: Person };
 }
-
-const roleMap = {
-  AC: "Chair",
-  ACV: "Vice Chair",
-  GHSR: "Scientific Representative",
-  GHSRV: "Vice Scientific Representative",
-  GHM: "Manager",
-  GHFL: "Financial & Legal Representative",
-  GSC: "Grant Awarding Coordinator",
-  "SCC/WG4VL2": "Science Communication Coordinator & WG4 Vice 2",
-  WG1L: "Theory Workgroup Leader",
-  WG1V1: "Theory Workgroup Vice 1",
-  WG1V2: "Theory Workgroup Vice 2",
-  WG2L: "Tools Workgroup Leader",
-  WG2V1: "Tools Workgroup Vice 1",
-  WG2V2: "Tools Workgroup Vice 2",
-  WG3L: "Data Workgroup Leader",
-  WG3V1: "Data Workgroup Vice 1",
-  WG3V2: "Data Workgroup Vice 2",
-  WG4L: "Dissemination Workgroup Leader",
-  WG$V1: "Dissemination Workgroup Vice 1",
-};
 
 // groups: Leadership and coordination
 // or: action chair / grant holder / coordination / working groups
@@ -41,7 +18,7 @@ const PeopleList: NextPage<Props> = ({ roles }) => {
         return <Person role={role} roles={roles} />;
       })} */}
 
-      <Group name="Action Leaders">
+      <Group key="leaders" name="Action Management">
         <Person role="AC" label="action chair" roles={roles} />
         <Person role="ACV" label="vice action chair" roles={roles} />
         <Person role="GHSR" label="scientific representative" roles={roles} />
@@ -60,7 +37,7 @@ const PeopleList: NextPage<Props> = ({ roles }) => {
         <Person role="GHFL" roles={roles} /> */}
       </Group>
       {["Theory", "Tools", "Data", "Dissemination"].map((wp, i) => (
-        <Group name={`Workgroup ${i + 1}: ${wp}`}>
+        <Group key={wp} name={`Workgroup ${i + 1}: ${wp}`}>
           <Person role={`WG${i + 1}L`} label="chair" roles={roles} />
           <Person role={`WG${i + 1}V1`} label="vice chair 1" roles={roles} />
           <div className="Person" />
