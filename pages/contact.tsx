7dd41ba86ba40e2e6fda0063881994ec8d6380f2
Kasper Welbers <kasperwelbers@gmail.com>
2022-12-28 17:46:30 +0100

    prettier

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,15 +6,26 @@ const Contact: NextPage = ({}) => {
       <style jsx>{`
         .ContactForm {
           display: flex;
+          flex-direction: column;
           margin: auto;
           color: white;
           max-width: 400px;
           padding: 5rem 2rem 2rem 2rem;
+          text-align: justify;
+          hyptens: auto;
+        }
+        .Title {
+          text-align: center;
         }
 
         form {
           margin: auto;
+          box-sizing: border-box;
           width: 100%;
+          border: 1px solid white;
+          padding: 1rem 2rem;
+          border-radius: 5px;
+          font-size: 2rem;
         }
         form p {
           display: flex;
@@ -22,8 +33,34 @@ const Contact: NextPage = ({}) => {
           align-items: stretch;
           color: white;
         }
+        form input {
+          height: 3rem;
+          font-size: 2rem;
+        }
+        form textarea {
+          font-size: 2rem;
+        }
+        button {
+          cursor: pointer;
+          margin-top: 2rem;
+          height: 5rem;
+          font-size: 2.5rem;
+          background: var(--primary);
+          color: white;
+          border: 3px solid white;
+          border-radius: 5px;
+        }
+        button:hover {
+          background: var(--primary-light);
+          color: var(--primary);
+        }
       `}</style>
       <div className="ContactForm">
+        <h2 className="Title">Contact us</h2>
+        <p>
+          If you have any questions or comments about this project, please feel
+          free to reach out to us.{" "}
+        </p>
         <form name="contact" method="post" data-netlify="true">
           <input type="hidden" name="form-name" value="contact" />
           <p>
@@ -37,7 +74,7 @@ const Contact: NextPage = ({}) => {
 
           <p>
             <label>Message:</label>
-            <textarea name="message"></textarea>
+            <textarea rows={10} name="message"></textarea>
           </p>
           <p>
             <button type="submit">Send</button>
